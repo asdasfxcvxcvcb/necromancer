62cd56f976a4400c2d8400d5946798282b75422a
narrow <166942253+narrown@users.noreply.github.com>
2026-01-01 09:28:52 -0500

    ignore this, this was an attempt to fix my VSC

--- necromancer/src/App/Features/Chat/Chat.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../../../SDK/SDK.h"
-
-// Run chat spammer (call from main loop)
-void RunChatSpammer();
-
-// Called when local player kills someone
-void OnKill(const char* victimName);
--- necromancer/src/App/Features/amalgam_port/Ticks/Ticks.cpp
+++
@@ -1,5 +0,0 @@
-#include "Ticks.h"
-
-// Simplified Ticks implementation for projectile aimbot
-// All functionality is inline in the header
-// This file exists for compilation purposes
